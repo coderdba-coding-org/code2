ca92ace3f4b88d947f9b67907a36f13e7fec8bef
GowrishMallipattana <gowrish.mallipattana@target.com>
2020-03-10 16:26:19 +0530

    updates

--- prometheus/query-api/api-query1.sh
+++ prometheus/query-api/api-query1.sh
@@ -6,4 +6,10 @@ curl 'http://localhost:9090/api/v1/query?query=up&time=2015-07-01T20:10:51.781Z'
 curl 'http://localhost:9090/api/v1/query?query=kube_pod_container_resource_requests_cpu_cores&time=2015-07-01T20:10:51.781Z' --> for specific timestamp
 curl 'http://localhost:9090/api/v1/query?query=kube_pod_container_resource_requests_cpu_cores' --> for the latest
 
+# Filters with URL encoding
+# https://stackoverflow.com/questions/54134924/prometheus-query-and-api-call
+curl -G \
+--data-urlencode 'query=kube_pod_container_resource_requests_cpu_cores{storeloc="EAST"} offset 5m' \
+https://fed-prometheus-server.company.com/api/v1/query
+
 
